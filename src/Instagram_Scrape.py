8519d54037bfb9af2c741025ec89e92fc660789c
antide-xx <antide_xx@protonmail.com>
2021-08-03 00:27:40 -0400

    comments

--- src/Instagram_Scrape.py
+++ src/Instagram_Scrape.py
@@ -6,22 +6,26 @@ from bs4 import BeautifulSoup
 import time
 import pandas as pd
 
-
+## Function takes list of usernames, check if username has profile on instagram ##
+## Returns pandas dataframe with username, if profile is found ##
 def username_check(username_list):
+	## initiate web driver ##
 	chrome_options = Options()
 	chrome_options.add_argument('--headless')
+	## uncomment this line for brave vvv
 	# chrome_options.binary_location= r'path to brave.exe'
 	driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
 	username_list=pd.read_csv(username_list)
 	usernamel=[]
 	usernamevaluelist=[]
+	## iterate through username list
 	for username in username_list["0"]:
-		
+		# go to page
 		driver.get("https://www.instagram.com/"+username)
 		time.sleep(3)
 		soup = BeautifulSoup(driver.page_source, 'html.parser')
 		title=soup.find("title").get_text()
-
+		## If page not found is returned, value is not found, otherwise is found
 		if "Page Not Found" in title:
 			usernamevalue='not found'
 		else:
@@ -29,3 +33,6 @@ def username_check(username_list):
 		usernamel.append(username)
 		usernamevaluelist.append(usernamevalue)
 		print (username, usernamevalue)
+	df=pd.DataFrame({"Username":usernamel,"IG Profile":usernamevaluelist})
+	return(df)
+		
