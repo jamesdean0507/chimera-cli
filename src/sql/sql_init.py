9b69daa1e0daff670cda91622e967d0442b32a78
antide-xx <antide_xx@protonmail.com>
2021-09-15 14:21:07 -0400

    minor fix

--- src/sql/sql_init.py
+++ src/sql/sql_init.py
@@ -4,20 +4,23 @@ import pandas as pd
 
 class database:
     
-    con = init_db()
+    
     
     def init_db():
         con = sqlite3.connect('main_db.db')
+        print(con)
         # cur=con.cursor()
         # cur.execute()
-        
+        return(con)
+      
     def import_df(csv):
         df=pd.read_csv(csv)
         # import df inidividually, and then cat all csv to one main
+        return(df)
         
     
-    def import_gdf(shpefle):
-        
+    # def import_gdf(shpefle):
+    #  ##   
         
     def query(con):
         query_df=pd.read_sql_query()
@@ -26,5 +29,6 @@ class database:
     def close_db(con):
         con.close()
     
-    
 
+if __name__=="__main__":
+    database.init_db()
